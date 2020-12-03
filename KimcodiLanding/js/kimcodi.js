e519a3dd1052b6e17dc70bd0fe4a2e68daa40745
misunssongeduhash <misunssongeduhash@eduhashglobal.com>
2020-12-03 11:45:39 +0900

    style: Appy page design feedback
    * 서울캠퍼스 랜딩페이지 수정요청항 적용

--- KimcodiLanding/js/kimcodi.js
+++ KimcodiLanding/js/kimcodi.js
@@ -5,55 +5,14 @@ function tabWrap(){
     $('.tabCon.first').show();
 
     tabLi.click(function(e){
-        e.preventDefault();
-        var $this = $(this);
-        if(tabLi.parents('.tabWrap').hasClass('kpaGwangju')){
-          if($this.index()>=4){
-            return false;
-          }else{
-            var $tabCon = $this.parents('.tab').siblings('.tabCon');
-            var $thisIndex = $(this).index();
-            $this.siblings('li').removeClass('on');
-            $this.addClass('on');
-            $tabCon.hide();
-            $tabCon.eq($thisIndex).show();
-            if($this.parents('.tabWrap').hasClass('tabStyle03')){
-              $tabCon.eq($thisIndex).find('.linked-tab').find('li').removeClass('on');
-              $tabCon.eq($thisIndex).find('.linked-tab').find('li').eq($thisIndex).addClass('on');
-            }
-          }
-        }else{
-          if($this.index()>=5){
-            return false;
-          }else{
-            var $tabCon = $this.parents('.tab').siblings('.tabCon');
-            var $thisIndex = $(this).index();
-            $this.siblings('li').removeClass('on');
-            $this.addClass('on');
-            $tabCon.hide();
-            $tabCon.eq($thisIndex).show();
-            if($this.parents('.tabWrap').hasClass('tabStyle03')){
-              $tabCon.eq($thisIndex).find('.linked-tab').find('li').removeClass('on');
-              $tabCon.eq($thisIndex).find('.linked-tab').find('li').eq($thisIndex).addClass('on');
-            }
-          }
-        }
-    });
-}
-
-function tabWrap02(){
-    var tabLi = $('.linked-tab li');
-    tabLi.click(function(e){
-        e.preventDefault();
-        var $this = $(this);
-        var $tabCon = $this.parents('.tabCon').siblings('.tab').siblings('.tabCon');
-        var $thisIndex = $(this).index();
-        $this.parents('.tabCon').siblings('.tab').find('li').removeClass('on');
-        $this.parents('.tabCon').siblings('.tab').find('li').eq($thisIndex).addClass('on');
-        $tabCon.hide();
-        $tabCon.eq($thisIndex).show();
-        $tabCon.eq($thisIndex).find('.linked-tab').find('li').removeClass('on');
-        $tabCon.eq($thisIndex).find('.linked-tab').find('li').eq($thisIndex).addClass('on');
+      e.preventDefault();
+      var $this = $(this);
+      var $tabCon = $this.parents('.tab').siblings('.tabCon');
+      var $thisIndex = $(this).index();
+      $this.siblings('li').removeClass('on');
+      $this.addClass('on');
+      $tabCon.hide();
+      $tabCon.eq($thisIndex).show();
     });
 }
 
@@ -66,12 +25,5 @@ $(function(){
   if($('.tabWrap').length > 0){
     tabWrap();
   }
-
-  if($('.linked-tab').length > 0){
-    tabWrap02();
-  }
-
-
-
 
 });
